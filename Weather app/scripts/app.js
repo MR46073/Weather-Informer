3ce24dc1847cc8626a412b25f504ceeb36fca132
MR46073 <99308917+MR46073@users.noreply.github.com>
2022-05-06 10:59:39 +0530

    Update app.js

--- Weather app/scripts/app.js
+++ Weather app/scripts/app.js
@@ -1,4 +1,4 @@
-const cityForm = document.querySelector('form'); //Here we could have used the class change-location, but since there is a single form on the entire page we are using the form directly here.
+const cityForm = document.querySelector('form');
 
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
@@ -6,16 +6,9 @@ const time = document.querySelector('img.time');
 const icon = document.querySelector('.icon img');
 
 const updateUi = (data) => {
-    
-    //const cityDets = data.cityDets;
-    //const weather = data.weather;
-    console.log(data);
-
-    // Destructure way
 
     const {cityDets, weather} = data;
 
-    // update details template
     details.innerHTML = `
         <h5 class="my-3">${cityDets.EnglishName}</h5>
         <div class="my-3">${weather.WeatherText}</div>
@@ -29,8 +22,6 @@ const updateUi = (data) => {
         card.classList.remove('d-none');
     }
     
-    // Update day & night And icon images
-    
     const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
     icon.setAttribute('src', iconSrc);
     
@@ -43,11 +34,10 @@ const updateUi = (data) => {
 
 const updateCity = async (city) => {
 
-    const cityDets = await getCity(city); //getting the city details from the api website according to what city keywords we have entered.
-    const weather = await getWeather(cityDets.Key);//getting the weather details of the city we want by taking that city's Key as parameter and giving us weather info of that city.
+    const cityDets = await getCity(city);
+    const weather = await getWeather(cityDets.Key);
 
     return {cityDets, weather};
-    // So the cityDets is basically city details (dets is shortfrm for details) AND weather is weather details. We converted them to objects here. Originally they were in json form.
 
 };
 
@@ -62,4 +52,4 @@ cityForm.addEventListener('submit', e => {
         .then(data => updateUi(data))
         .catch(err => console.log(err));
 
-}); //the data mentioned here is whatever we are returning in the updateCity function i.e. the object converted data.
+});
